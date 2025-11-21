c322a54cc42bdb4a0e07e4780a01dc7c64c1ca3a
Matisse08 <95353974+Matisse08@users.noreply.github.com>
2025-11-21 22:54:06 +0100

    Final

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from . import models, database
-from .routers import auth, indicators, dashboard, users  # <--- Ajout de 'dashboard'
+# Ajout de users et zones dans les imports
+from .routers import auth, indicators, dashboard, users, zones 
 
 # Création des tables dans la BDD au démarrage
 models.Base.metadata.create_all(bind=database.engine)
@@ -9,14 +10,11 @@ app = FastAPI(title="EcoTrack API")
 
 # Inclusion des routeurs
 app.include_router(auth.router)
+app.include_router(users.router)       # <-- Nouveau routeur
+app.include_router(zones.router)       # <-- Nouveau routeur
 app.include_router(indicators.router)
-app.include_router(dashboard.router) # <--- Activation de la route dashboard
+app.include_router(dashboard.router)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to EcoTrack API. Go to /dashboard to see the UI."}
-
-app.include_router(auth.router)
-app.include_router(indicators.router)
-app.include_router(users.router)
-app.include_router(dashboard.router)
